827bd91aefd917cfbc42191f975d42f3554270f3
SystematicError <systematicerror@users.noreply.github.com>
2024-01-23 18:22:29 +0000

    feat: show battery percentage

--- bar/modules/power.js
+++ bar/modules/power.js
@@ -2,10 +2,18 @@ import Widget from "resource:///com/github/Aylur/ags/widget.js"
 import Battery from "resource:///com/github/Aylur/ags/service/battery.js"
 
 function Power() {
-    return Widget.Icon({
-        class_name: "battery",
-        size: 16,
-        icon: Battery.bind("icon_name")
+    return Widget.Box({
+        children: [
+            Widget.Icon({
+                class_name: "battery",
+                size: 16,
+                icon: Battery.bind("icon_name")
+            }),
+
+            Widget.Label({
+                label: Battery.bind("percent").transform(p => `${p}%`)
+            })
+        ]
     })
 }
 
